9fffd933e0bfbde3968dceeec2025a0f4161e60d
FARIZ MURADOV <aze2201@gmail.com>
2021-03-15 11:30:44 +0100

    Update voted.sh

--- cgi-bin/voted.sh
+++ cgi-bin/voted.sh
@@ -4,18 +4,23 @@
 echo "Content-type: text/html"
 echo 
 echo "<script src=\"../js/getsetcookie.js\"></script>"   
+
 cookieUsername=$(echo ${HTTP_COOKIE} | awk -v FS='username=' '{print $2}')
 
 if [ "$REQUEST_METHOD" = POST ]; then
     query=$( head --bytes="$CONTENT_LENGTH" )
 fi
-
+#echo $query
 
 mkdir /tmp/votef
-#echo $query
+
+# parse POSTed image name
 imageVoted=$(echo ${query}| awk -v FS='imagevoted=' '{print $2}')
 
-echo "$imageVoted" > /tmp/votef/$cookieUsername.voted
+if [ ! -z $cookieUsername ]; then
+  echo "$imageVoted" > /tmp/votef/$cookieUsername.voted
+fi
+
 echo "
       <center>
 	     <H1>Thank you.<br>Democracy wins</H1>
@@ -23,7 +28,7 @@ echo "
 		 count  | NAME<br>
 		 $(cat  /tmp/votef/*.voted | sort | uniq -c |tr '\n' '#' | sed 's/#/<br>/g'| tr ' ' '|' | sed 's/||||||//g')
 		 <br><br>
-		 NOTE: <a href=\"http://192.168.1.195:8000/cgi-bin/list.sh\">You can change your vote or back to see list again</a>
+		 NOTE: <a href=\"http://192.168.1.x:8000/cgi-bin/list.sh\">You can change your vote or back to see list again</a>
       </center>"
-
+      
 exit 0
