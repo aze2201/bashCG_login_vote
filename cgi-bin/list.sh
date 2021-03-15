c6d19a7300ec14bd920150f54eeba0e640df5266
FARIZ MURADOV <aze2201@gmail.com>
2021-03-15 11:28:27 +0100

    Update list.sh

--- cgi-bin/list.sh
+++ cgi-bin/list.sh
@@ -2,26 +2,29 @@
  
 echo "Content-type: text/html"
 echo ""
-#http://192.168.1.195:8000/cgi-bin/vote.sh
 
 echo " <!DOCTYPE html> "
 echo " <html>          "
 echo " <head>          "
+# custom JS functions for Cookie
 echo "<script src=\"../js/getsetcookie.js\"></script>"       
 echo " </head>         "
 echo " <body>          "
 
+# extract Username (if more cookie set it could fail. Need to check parsing properly)
 cookieUsername=$(echo ${HTTP_COOKIE} | awk -v FS='username=' '{print $2}')
 
+# if cookie is set.
 if [ ! -z "$cookieUsername"  ]; then
     echo "<center><H2>Hi $cookieUsername <br><br>
-              Welcome to VOTE
-          </H2><br>
-          <h3>
+            Welcome to VOTE
+            </H2><br>
+            <h3>
     	      Please select the RADIO BUTTON on the image below and click on the SUBMIT button at the bottom of the page
-          </h3>"
-    echo "<h1>Picture list: </h1><br>"
+           </h3>"
+    echo  "<h1>Picture list: </h1><br>"
     
+    # put all processing part into FORM
     echo "<FORM ACTION=/cgi-bin/voted.sh METHOD=POST >"
     for i in $(ls /home/muradovf/f/images/*.jpg); do
       echo "
@@ -38,7 +41,7 @@ if [ ! -z "$cookieUsername"  ]; then
     echo "<input type=submit value=Submit>"
 else
     echo "<h2>Please LOGIN</h2>"
-	echo "<a href=\"http://192.168.1.195:8000/login.html\">Login page</a>"
+	echo "<a href=\"http://192.168.1.x:8000/login.html\">Login page</a>"
 fi
 
 
